8c7ba8e1901b2cc4367dd2265b1ccff98b0c4778
Manmeet Kaur <jimmy06c206@gmail.com>
2020-07-29 23:13:51 +0530

    Initial Comment

---
+++ src/test/java/WebDriverMaven.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class WebDriverMaven {
+	
+	@Test
+	public void doLogin(){
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://gmail.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);	
+		
+		driver.findElement(By.id("identifierId")).sendKeys("jimmy06c206@gmail.com");
+		//driver.findElement(By.id("nextiId")).click();
+		
+		
+		driver.quit();
+		
+	}
+}
+		
+		
