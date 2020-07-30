961bd7c665b9e3fe83b71f23f48cc44194081362
Manmeet Kaur <jimmy06c206@gmail.com>
2020-07-30 14:51:03 +0530

    First Jenkins Commit

--- src/test/java/WebDriverMaven.java
+++ src/test/java/WebDriverMaven.java
@@ -2,7 +2,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class WebDriverMaven {
@@ -10,13 +10,13 @@ public class WebDriverMaven {
 	@Test
 	public void doLogin(){
 		
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
 		driver.get("http://gmail.com");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);	
 		
 		driver.findElement(By.id("identifierId")).sendKeys("jimmy06c206@gmail.com");
-		driver.findElement(By.id("nextiId")).click();
+		//driver.findElement(By.id("nextiId")).click();
 		
 		
 		driver.quit();
